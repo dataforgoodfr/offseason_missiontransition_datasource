95b41f1c264c618ba584e84e2d4f515cc95d9c28
Nathan <nathan.lauga@gmail.com>
2022-01-06 19:13:42 +0100

    Get aides.csv and add docstring in the script

--- scripts/scrap_pdf_files.py
+++ scripts/scrap_pdf_files.py
@@ -12,33 +12,77 @@ import PyPDF2
 from datasource.io import get_data_from_url
 from datasource.utils import get_logger
 
-
+# DEFINING GLOBAL VARIABLES
 API_URL = "https://aides-territoires.beta.gouv.fr/api/aids/"
 CRITERIA_WORDS = ["conditions", "critères", "éligible", "éligibilité"]
 logger = get_logger("SCRAP_PDF_FROM_API")
+NUM_SCRAP = 1
 
 
 def is_url_working(url, logger):
+    """Checks that url is working or not.
+
+    Controls:
+    - url start with http
+    - Status code not 200
+    - Url redirect into another one
+    - also catch unexpected error
+
+    Parameters
+    ----------
+    url : str
+        Url to check
+    logger : Logger
+        logger to display information
+
+    Returns
+    -------
+    bool
+        Whether the url is valid or not
+    str
+        string explaining the problem
+    """
+
+    if not url.startswith("http"):
+        return False, "url not starting with http"
 
     logger.info("%s" % url)
+
     try:
         resp = get_data_from_url(url)
     except Exception as e:
         logger.error(e)
-        return False, None
+        return False, "Unknown error"
 
     if resp.status_code != 200:
         logger.error("Status code not 200, instead %i" % resp.status_code)
-        return False, None
+        return False, "Status %i" % resp.status_code
 
     if resp.url != url:
         logger.error("url changed to %s" % resp.url)
-        return False, None
+        return False, "url changed to %s" % resp.url
 
     return True, resp
 
 
 def get_pdf_content_from_url(pdf_url):
+    """Extract all the text from a pdf url
+
+    Parameters
+    ----------
+    pdf_url : str
+        url of the pdf
+
+    Returns
+    -------
+    str
+        string of all the PDF content
+
+    Raises
+    ------
+    ValueError
+        pdf_url must ends with '.pdf'
+    """
 
     if not pdf_url.endswith(".pdf"):
         raise ValueError("pdf_url must ends with '.pdf'")
@@ -57,18 +101,22 @@ def get_pdf_content_from_url(pdf_url):
     return full_text
 
 
-def scrap_pdf_in_url(url, logger):
+def scrap_pdf_in_url(resp):
+    """Find all pdf in a html page and scrap them
 
-    if not url.startswith("http"):
-        return []
+    Parameters
+    ----------
+    resp :
+        requests response of an url
 
-    is_ok, resp = is_url_working(url, logger)
-
-    if not is_ok:
-        return []
+    Returns
+    -------
+    list
+        List of pdf find in an url
+    """
 
     # pdf_urls = re.findall(r"http.*\.pdf", resp.text)
-    pdf_urls = re.findall(r"(?:(?!\").)*?\.pdf", resp.text)
+    pdf_urls = re.findall(r"(?:(?!\"|').)*?\.pdf", resp.text)
 
     if len(pdf_urls) == 0:
         return []
@@ -77,7 +125,8 @@ def scrap_pdf_in_url(url, logger):
 
     for pdf_url in set(pdf_urls):
         if pdf_url.startswith("/"):
-            domain = urlparse(url).netloc
+            parsed_url = urlparse(resp.url)
+            domain = parsed_url.netloc
             pdf_url = "http://" + domain + pdf_url
 
         logger.info("PDF Analyse : %s" % pdf_url)
@@ -97,21 +146,50 @@ def scrap_pdf_in_url(url, logger):
 
 
 def get_one_aide_data(aide, logger):
+    """Retrieves all information about one "aide"
+    from the API defined in global
+
+    Parameters
+    ----------
+    aide : dict
+        One "aide" from the Mission transition API
+    logger :
+        Logger to display all informations
+
+    Returns
+    -------
+    dict
+        Result of the "aide" analysis
+    """
+    global NUM_SCRAP
     name = aide["name"]
     aide_url = aide["url"]
     orig_url = aide["origin_url"]
     app_url = aide["application_url"]
 
-    logger.info("Analyse %s" % (aide_url))
+    logger.info("[%i] - Analyse %s" % (NUM_SCRAP, aide_url))
+    NUM_SCRAP = NUM_SCRAP + 1
+
     current_aide = {"name": name, "url": aide_url}
 
     pdfs = []
 
     # check orig_url
-    pdfs += scrap_pdf_in_url(orig_url, logger)
+
+    is_ok, resp = is_url_working(orig_url, logger)
+    current_aide["error_orig"] = is_ok
+    if is_ok:
+        pdfs += scrap_pdf_in_url(resp)
+    else:
+        current_aide["type_error_orig"] = resp
 
     # check app_url
-    pdfs += scrap_pdf_in_url(app_url, logger)
+    is_ok, resp = is_url_working(app_url, logger)
+    current_aide["error_app"] = is_ok
+    if is_ok:
+        pdfs += scrap_pdf_in_url(resp)
+    else:
+        current_aide["type_error_app"] = resp
 
     current_aide["pdfs"] = pdfs
     current_aide["pdf_avec_criteres"] = False
@@ -122,12 +200,30 @@ def get_one_aide_data(aide, logger):
 
 
 def get_data_aides_results(data, logger):
+    """Analyse all "aides" from the data given
+    by the API
+
+    Parameters
+    ----------
+    data : dict
+        Return from the Mission transition API
+    logger :
+        Logger to display all informations
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
 
     aides_list = []
 
     for aide in data["results"]:
-        current_aide = get_one_aide_data(aide, logger)
-        aides_list.append(current_aide)
+        try:
+            current_aide = get_one_aide_data(aide, logger)
+            aides_list.append(current_aide)
+        except Exception as e:
+            logger.error(e)
 
     return aides_list
 
@@ -160,6 +256,7 @@ def scrap_current_api_page(url, logger=None):
 
 
 def srap_pdf():
+    """Main script to scrap and find pdfs given the differents url"""
     aides_list = []
 
     url, aides = scrap_current_api_page(API_URL, logger=logger)
@@ -169,10 +266,8 @@ def srap_pdf():
         url, aides = scrap_current_api_page(url, logger=logger)
         aides_list += aides
 
-        break
-
     df = pd.DataFrame(aides_list)
-    df.to_csv("aides.csv", index=False)
+    df.to_csv("data/aides.csv", index=False)
 
 
 if __name__ == "__main__":
